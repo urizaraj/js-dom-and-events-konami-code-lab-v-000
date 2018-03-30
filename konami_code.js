c0f1cd56a6fd36bbc9d528898dfb5edf436258df
Alexander Urizar <urizaraj@gmail.com>
2018-03-30 15:10:48 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,19 @@
+
+
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+let index = 0;
+const history = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 
-function init() {
-  // Write your JavaScript code inside the init() function
+function init(e) {
+  document.body.addEventListener('keydown', checkKC)
+}
 
+function checkKC(e) {
+  const key = parseInt(e.detail || e.which);
+  history.shift()
+  history.push(key)
+
+  if (history.join() === code.join()) {
+    alert('good job!')
+  }
 }
